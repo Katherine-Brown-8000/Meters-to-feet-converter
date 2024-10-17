c673fd69d98469078601e9b0fa6a85b3ae2d2824
Katherine Brown <155707632+Katherine-Brown-8000@users.noreply.github.com>
2024-10-16 23:40:51 -0400

    Create Meters to Feet Converter.py

---
+++ Meters to Feet Converter.py
@@ -0,0 +1,10 @@
+metric = str(input("Enter M for Meters or F for feet: "))
+distant = float(input("distance: "))
+
+
+if metric == 'M':
+    mf = distant * 3.2808399
+    print(f"distance in feet is {mf:.2f}")
+elif metric == 'F':
+    fm = distant*0.3048
+    print(f"distance in meters is {fm:.2f}")
